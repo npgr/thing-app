7da36cbb430b84f80170ad3d9f07aa89f454aceb
Nuno <ngocalves@gmail.com>
2019-08-18 22:10:39 +0200

    Configure Apollo Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
 import App from './js/containers/App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const link = createHttpLink({ uri: 'http://localhost:5000/graphql' })
+const cache = new InMemoryCache()
+const client = new ApolloClient({ link, cache })
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+)
