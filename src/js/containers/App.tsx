3fcce71a2d06753cd7991d84a1234620ade6f633
Nuno <ngocalves@gmail.com>
2019-08-20 00:25:30 +0200

    first draft of Authentication Page

--- src/js/containers/App.tsx
+++ src/js/containers/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
+import Auth from './Auth'
 import Explore from './Explore'
 import Detail from './Detail'
 
@@ -11,9 +12,8 @@ export default () => (
         <h3>Thing App</h3>
       </Menu.Header>
     </Menu>
-
-    <Route exact path='/' component={Explore} />
+    <Route exact path='/' component={Auth} />
+    <Route path='/explore' component={Explore} />
     <Route path='/detail/:id' component={Detail} />
-    <Redirect to='/' />
   </Router>
 )
